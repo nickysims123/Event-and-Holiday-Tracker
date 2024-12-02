051713941ea164fdc2e64242af2bd9b0aabb87e1
Nicholas Sima <nick.sima@outlook.com>
2024-12-02 18:31:33 -0500

    Environment setup, Dockerfile, entrypoint, requirements, and tests

---
+++ sql/create_meal_table.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS events;
+CREATE TABLE events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_day INTEGER NOT NULL,
+    event_month INTEGER NOT NULL,
+    event_year INTEGER NOT NULL,
+    event_name TEXT NOT NULL UNIQUE,
+    is_religious BOOLEAN NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE
+);
