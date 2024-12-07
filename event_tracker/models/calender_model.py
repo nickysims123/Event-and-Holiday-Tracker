c2277451124adef7b1e2228f31b236e96dadfaf5
yukapine <jackbpine@gmail.com>
2024-12-07 17:21:54 -0500

    added new folders, added placeholder functions

---
+++ event_tracker/models/calender_model.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+import logging
+import os
+import sqlite3
+from typing import Any
+import datetime
+
+
+
+
+@dataclass
+class Event:
+    id: int
+    event_name: str
+    date: str
+
+    def __post_init__(self):
+        ### check if date is valid/format date?
+        pass
+    
+def create_event(id: int, event_name: str, date: str) -> None:
+    pass
+
+def delete_event() -> None:
+    pass
+
+def check_distance(date: str) -> int:
+    """
+    Returns the distance in days between today and a given date.
+
+    Args:
+        date (str): the date to calculate the number of days until.
+
+    Raises: 
+        ValueError: if the date is not a string
+        ValueError: if the date is not formated correctly
+    """
+    if not isinstance(date, (str)):
+        raise ValueError(f"Invalid date: {date}. date must be a string in the format day/month/year.")
+    try:
+        datetime.date.fromisoformat(date)
+    except ValueError:
+        raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+    
+    today = datetime.date.today()
+    
+    date = format_date(date)
+
+    difference = datetime.strptime(today, "%Y/%m/%d") - datetime.strptime(date, "%Y/%m/%d") 
+
+    return int(difference.days)
+
+def check_if_holiday() -> bool:
+    pass
+
+def list_holidays_this_week() -> list:
+    pass
+
+def format_date(date: str) -> str:
+    #take in str and formats into proper date format?
+    pass
+
+
