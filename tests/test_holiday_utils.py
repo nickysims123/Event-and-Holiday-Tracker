77f4928af21242034b46690617f443b62ef46e00
yukapine <jackbpine@gmail.com>
2024-12-10 23:52:49 -0500

    fixed unit tests for holiday_utils

--- tests/test_holiday_utils.py
+++ tests/test_holiday_utils.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 
-from event_tracker.utils.random_utils import get_holiday_status
+from event_tracker.utils.holiday_utils import get_holiday_status
 
 HOLIDAY_STATUS = True
 
@@ -10,8 +10,8 @@ def mock_nager_at(mocker):
     # Patch the requests.get call
     # requests.get returns an object, which we have replaced with a mock object
     mock_response = mocker.Mock()
-    # We are giving that object a text attribute
-    mock_response.bool = True
+    mock_response.ok = True
+    mock_response.status_code = 200
     mocker.patch("requests.get", return_value=mock_response)
     return mock_response
 
@@ -30,20 +30,20 @@ def test_get_holiday_status_request_failure(mocker):
     """Simulate  a request failure."""
     mocker.patch("requests.get", side_effect=requests.exceptions.RequestException("Connection error"))
 
-    with pytest.raises(RuntimeError, match="Request to nager.at failed: Connection error"):
+    with pytest.raises(RuntimeError, match="Request to date.nager.at failed: Connection error"):
         get_holiday_status()
 
 def test_get_holiday_status_timeout(mocker):
     """Simulate  a timeout."""
     mocker.patch("requests.get", side_effect=requests.exceptions.Timeout)
 
-    with pytest.raises(RuntimeError, match="Request to random.org timed out."):
+    with pytest.raises(RuntimeError, match="Request to date.nager.at timed out."):
         get_holiday_status()
 
 def test_get_holiday_status_invalid_response(mock_nager_at):
     """Simulate  an invalid response (non-bool)."""
-    mock_nager_at.text = "invalid_response"
+    mock_nager_at.ok = 'invalid'
 
-    with pytest.raises(ValueError, match="Invalid response from nager.at: invalid_response"):
+    with pytest.raises(ValueError, match="Invalid response from random.org: invalid_response"):
         get_holiday_status()
 
